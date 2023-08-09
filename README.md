33ec5ff9f4681ab850c00024eb72950e45600b36
lyndskg <lyndskg@umich.edu>
2023-08-09 08:24:14 -0500

    Hi

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # uwu-bot
 A Twitter chatbot built w/ GPT3 and Replit that replies in UwU -_-
+
+
+### <ins>Prerequisites</ins>
+> - `Python`: v.*3.11+*
+> - `tweepy`: v.*4.14.0+*
+> - `oauthlib`: v.*3.2.2*
+> - `requests-oauthlib`: v.*1.3.1*
+> - `aiohttp`: v.*3.8.5*
+> - `aiosignal`: v.*1.3.1*
+> - `async-timeout`: v.*4.0.2*
+> - `frozenlist`: v.*1.4.0*
+> -  `multidict`: v.*6.0.4*
+> - `openai`: v.*0.27.8*
+> - `tqdm`: v.*4.66.0*
+> - `yarl`: v.*1.9.2*
+> - `Pillow`: v.*10.0.0*
+> - `requests`: v.*2.31.0*
--- main.py
+++ main.py
@@ -1,1 +1,94 @@
+import tweepy
+from io import BytesIO
+from Pillow import Image, ImageDraw, ImageFont
+import os
+import requests
+import random
+import time
+import sys
+import openai
+import textwrap
+import logging
 
+
+# Configure the logger
+logging.basicConfig(
+    filename='uwu.log',  # Specify the log file name
+    level=logging.DEBUG, # Set the logging level to DEBUG or higher
+    format='[%(levelname)s] %(asctime)s - %(message)s' # Specify the log message format
+)
+logger = logging.getLogger(__name__)
+
+openai.api_key = "xxx"
+
+
+#Accessing credentials from .env file
+CONSUMER_KEY = "xxx"
+CONSUMER_SECRET = "xxx"
+ACCESS_TOKEN = "xxx"
+ACCESS_TOKEN_SECRET = "xxx"
+
+
+#Setting credentials to access Twitter API
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+#Calling API using Tweepy
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+
+#Search keyword 
+# Got them from https://twitter-trends.iamrohit.in/kenya/nairobi
+search = '#UnfairKECOBOTerms OR #TaxTheRich OR Safaricom OR KCSE #andrewkibe OR #MasculinitySaturday OR #SavanisBookCentre OR #TheBookshopOfYourChoice'
+# Maximum limit of tweets to be interacted with
+maxNumberOfTweets = 1000000
+
+# To keep track of tweets published
+count = 0
+
+
+print("Retweet Bot Started!")
+logger.info("Retweet Bot Started!")
+
+
+for tweet in tweepy.Cursor(api.search_tweets, search).items(maxNumberOfTweets):
+    try:
+        # for each status, overwrite that status by the same status, but from a different endpoint.
+        status = api.get_status(tweet.id, tweet_mode='extended')
+        if status.retweeted == False and status.favorited == False:
+            print("###############################################################")
+            print("Found tweet by @" + tweet.user.screen_name)
+            tweet.favorite()
+            print("Liked tweet")
+            tweet.retweet()
+            print("Retweeted tweet")
+
+            prompt = textwrap.shorten(tweet.text, width=280)
+            # Use the tweet text as the prompt for ChatGPT
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens = 80,
+                n = 1,
+                stop=None,
+                temperature=0.7
+            )
+            reply_text = response["choices"][0]["text"]
+
+            # Send the reply
+            api.update_status('@'+tweet.user.screen_name+''+reply_text, in_reply_to_status_id=tweet.id)
+            print("Replied to tweet")
+            print("###############################################################")
+
+            #Random wait time
+            timeToWait = random.randint(95, 115)
+            print("Waiting for "+ str(timeToWait) + " seconds")
+            for remaining in range(timeToWait, -1, -1):
+                sys.stdout.write("\r")
+                sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+                sys.stdout.flush()
+                time.sleep(1)
+            sys.stdout.write("\rOnwards to next tweet!\n")
+
+    except tweepy.errors.TweepyException as e:
+        print(str(e))
